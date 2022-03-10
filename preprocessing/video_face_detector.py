91fe1409ab4c5e591d0ce65c75bea62e2246a103
Vyacheslav Zhdanov <55495044+Cyber-Tapok@users.noreply.github.com>
2022-03-10 23:18:37 +0400

    Update video_face_detector.py

--- preprocessing/video_face_detector.py
+++ preprocessing/video_face_detector.py
@@ -1,10 +1,10 @@
-from image_face_detector import ImageFaceDetector
-from frame_extractor import FrameExtractor
+from .image_face_detector import ImageFaceDetector
+from .frame_extractor import FrameExtractor
 
 class VideoFaceDetector:
 
-  def __init__(self):
-    self.imageDetector = ImageFaceDetector()
+  def __init__(self, predictor_path):
+    self.imageDetector = ImageFaceDetector(predictor_path=predictor_path)
     self.videoExtractor = FrameExtractor()
 
   def preprocess_video(self, video: str, targetFramerate=30.0, targetFrames = None, callback = None):
