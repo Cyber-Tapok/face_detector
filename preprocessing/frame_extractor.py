3f623f735469adedef4eb3d3ffa9a30190710be2
Vyacheslav Zhdanov <55495044+Cyber-Tapok@users.noreply.github.com>
2022-03-20 12:29:15 +0400

    Update frame_extractor.py

--- preprocessing/frame_extractor.py
+++ preprocessing/frame_extractor.py
@@ -1,19 +1,23 @@
 from cv2 import cv2
+from os import path
 
 
 class FrameExtractor:
 
-  def _write_image(self, savePath, image, id):
-    cv2.imwrite(f"{savePath}/frame%d.jpg" % id, image)
+  def _write_image(self, savePath, image, id, videoname):
+    cv2.imwrite(f"{savePath}/{videoname}-{id}.jpg", image)
 
 
   def extract_from(self, video: str, targetFramerate: float = 30.0, targetFrames: int = None, savePath: str = None):
     vidcap = cv2.VideoCapture(video)
     
+    basename = path.basename(video)
+    videoname = path.splitext(basename)[0]
+
     frames = self._extract(vidcap, targetFramerate, targetFrames)
-    if(savePath is not None):
+    if savePath is not None:
       for index, frame in enumerate(frames):
-         self._write_image(savePath, frame, index)
+         self._write_image(savePath, frame, index, videoname)
 
     vidcap.release()
     return frames
@@ -28,7 +32,7 @@ class FrameExtractor:
     interval = targetFramerate / vidcap.get(cv2.CAP_PROP_FPS)
 
     while success:
-      if(int(count) > oldPosition):
+      if int(count) > oldPosition :
         oldPosition = int(count)
         extractedFrames.append(image)
 
@@ -36,7 +40,7 @@ class FrameExtractor:
       
       count += interval
 
-      if(targetFrames == oldPosition and targetFrames is not None):
+      if targetFrames == oldPosition and targetFrames is not None:
         break
       
     print("Total extracted frames:", len(extractedFrames))
